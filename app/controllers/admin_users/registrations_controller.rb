35bb0499228d8ea2c513c2f2ff90b5ee1fa0d3f9
MamoonaT <mamoonatahir17@gmail.com>
2022-08-31 17:44:55 +0500

    permitting parameters for devise

--- app/controllers/admin_users/registrations_controller.rb
+++ app/controllers/admin_users/registrations_controller.rb
@@ -3,6 +3,15 @@
 class AdminUsers::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    attributes = [:first_name, :email]
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:first_name, :email])
+  end
+
 
   # GET /resource/sign_up
   # def new
